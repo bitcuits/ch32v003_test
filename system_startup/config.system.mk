bcd2b65bf158ff3d82e7ee0b6843d58234f91751
cbleser <cr@tagion.org>
2024-12-07 15:19:46 +0200

    WIP: grouping build config.system.mk and config.tools.mk added

---
+++ system_startup/config.system.mk
@@ -0,0 +1,32 @@
+LIBROOT?=framework-wch-noneos-sdk/
+#System/ch32v00x/system_ch32v00x.c
+
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_adc.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_dbgmcu.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_dma.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_exti.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_flash.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_gpio.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_i2c.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_iwdg.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_misc.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_opa.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_pwr.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_rcc.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_spi.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_tim.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_usart.c
+LIBSRC+=Peripheral/ch32v00x/src/ch32v00x_wwdg.c
+
+LIBSRC+=Core/ch32v00x/core_riscv.c
+LIBSRC+=System/ch32v00x/system_ch32v00x.c
+LIBASM+=Startup/startup_ch32v00x.S
+
+LIBOBJ:=$(LIBSRC:.c=.o)
+LIBOBJ+=$(LIBASM:.S=.o)
+LIBOBJ:=$(addprefix $(BUILD)/,$(LIBOBJ))
+#LIBSRC:=$(addprefix $(LIBROOT)/,$(LIBSRC))
+
+LIBFILE:=$(BUILD)/libch32v00x.a
+
+
